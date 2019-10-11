46524345deff2aeccdf51180f64426502c5e13d2
Klypalskyi <klypalskyi@gmail.com>
2019-10-11 21:10:32 +0300

    Updates

---
+++ precache-manifest.33a1a5bd7ecaf4069027d745cbcc5b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a26468ac116556cfaa7d67b921767b",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "fc9df2f8b9594b1b410b",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "258aceb70c9a7da46e36",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.a12b5a91.chunk.css"
+  },
+  {
+    "revision": "fc9df2f8b9594b1b410b",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.48117e80.chunk.js"
+  },
+  {
+    "revision": "258aceb70c9a7da46e36",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.41899b1a.chunk.js"
+  },
+  {
+    "revision": "3abcae2be7dde6a72721",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.a2b09cc2.js"
+  }
+]);
